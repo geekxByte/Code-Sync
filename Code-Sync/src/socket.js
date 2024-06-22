3076d151ff26264b81a312b6070808881b24088c
Deepak Mishra <115074475+geekxByte@users.noreply.github.com>
2024-06-22 20:44:47 +0530

    Update socket.js

--- Code-Sync/src/socket.js
+++ Code-Sync/src/socket.js
@@ -1,12 +1,17 @@
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
 
 export const initSocket = async () => {
     const options = {
-        'force new connection': true,
-        reconnectionAttempt: 'Infinity',
+        reconnectionAttempts: Infinity,
         timeout: 10000,
         transports: ['websocket'],
     };
 
-    return io(process.env.REACT_APP_BACKEND_URL, options);
+    try {
+        const socket = io(process.env.REACT_APP_BACKEND_URL, options);
+        return socket;
+    } catch (error) {
+        console.error('Socket connection error:', error);
+        throw error; // Propagate the error to handle it elsewhere if needed
+    }
 };
