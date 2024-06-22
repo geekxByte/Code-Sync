5286fc7b55ab9953b39c5200342d6f5cac0680ba
Deepak Mishra <115074475+geekxByte@users.noreply.github.com>
2024-06-22 22:17:10 +0530

    Update socket.js

--- Code-Sync/src/socket.js
+++ Code-Sync/src/socket.js
@@ -1,30 +1,17 @@
-// src/socket.js
-
 import { io } from 'socket.io-client';
 
-export const initSocket = (roomId, username) => {
-  const options = {
-    reconnectionAttempts: Infinity,
-    timeout: 10000,
-    transports: ['websocket'],
-  };
+export const initSocket = async () => {
+    const options = {
+        reconnectionAttempts: Infinity,
+        timeout: 10000,
+        transports: ['websocket'],
+    };
 
-  const socket = io(process.env.REACT_APP_BACKEND_URL, options);
-
-  // Handling socket connection errors
-  socket.on('connect_error', (err) => {
-    console.error('Socket connection error:', err);
-    throw new Error('Socket connection failed');
-  });
-
-  // Joining room on socket connection
-  socket.on('connect', () => {
-    socket.emit(ACTIONS.JOIN, {
-      roomId,
-      username,
-    });
-  });
-
-  return socket;
+    try {
+        const socket = io(process.env.REACT_APP_BACKEND_URL, options);
+        return socket;
+    } catch (error) {
+        console.error('Socket connection error:', error);
+        throw error; // Propagate the error to handle it elsewhere if needed
+    }
 };
-
